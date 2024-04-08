a7be2c6bfbbebdb43f98caaa9f98b5a533ddef11
rikcav <henriquearau000@gmail.com>
2024-04-08 16:59:22 -0300

    feat: email controller with endpoint to send emails

---
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -0,0 +1,31 @@
+package com.ms.email.controllers;
+
+import com.ms.email.dtos.EmailDTO;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+    private final EmailService emailService;
+
+    @Autowired
+    EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/sendindEmail")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDTO emailDTO) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDTO, emailModel);
+        EmailModel savedEmail = emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(savedEmail, HttpStatus.CREATED);
+    }
+}
